140c9c27e7ce656a76bf829608084f8c95a81dd0
Grzegorz Leoniec <greg@app-interactive.de>
2023-03-31 11:33:05 +0200

    update config

--- .tina/config.ts
+++ .tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
   branch,
-  clientId: 'f65d1093-c8ef-4ac8-b5b5-8feed1b1e53a', // Get this from tina.io
-  token: 'bfee0879e798e52b832908f9978997ccb67f4fc7', // Get this from tina.io
+  clientId: 'fada5bbf-afba-44c3-9820-739cae10698c', // Get this from tina.io
+  token: '1aa7f4492f69a341d7bf6560b3147e48f5204cb4', // Get this from tina.io
   build: {
     outputFolder: 'admin',
     publicFolder: 'public',
