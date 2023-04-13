9f38f504505546f895b9ffa5dddb5c8aba85317c
Grzegorz Leoniec <greg@app-interactive.de>
2023-04-13 20:16:01 +0200

    improved navigation popup animation

--- src/components/layout/MenuPopover.tsx
+++ src/components/layout/MenuPopover.tsx
@@ -48,11 +48,11 @@ export default function MenuPopover({ label, items, isMultiLevel = false }) {
       <Transition
         as={Fragment}
         enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
+        enterFrom="opacity-0 -translate-y-2 scale-[0.98]"
+        enterTo="opacity-100 translate-y-0 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="opacity-100 translate-y-0 scale-100"
+        leaveTo="opacity-0 -translate-y-2 scale-[0.98]"
       >
         <Popover.Panel
           className={`absolute left-1/2 z-10 mt-2 flex w-screen max-w-max px-4 drop-shadow-xl ${
