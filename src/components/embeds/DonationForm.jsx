d1f407ed8ae0b33e8d0cfbc35323a6f84fc6cdf3
Grzegorz Leoniec <greg@app-interactive.de>
2023-04-20 10:48:20 +0200

    fixed donation form height

--- src/components/embeds/DonationForm.jsx
+++ src/components/embeds/DonationForm.jsx
@@ -7,17 +7,17 @@ export default function DonationForm() {
   }, [])
 
   return (
-    <div className="h-[2000px] w-full sm:h-[1700px]">
+    <div className="h-[2050px] !w-full sm:h-[1800px]">
       {!shouldRender && <p className="p-20">Lade Spendenformular...</p>}
       {shouldRender && (
         <iframe
           src="https://secure.fundraisingbox.com/app/payment?hash=6j98n4k8fqegir0h#https%3A%2F%2Fwww.grenzensindrelativ.de%2Fspenden.html"
           title="Spendenformular"
-          className="h-[2000px] w-full sm:h-[1700px]"
+          className="h-[2050px] !w-full sm:h-[8700px]"
           allow="payment"
           frameBorder={0}
         >
-          <div className="h-[2000px] w-full p-20 sm:h-[1700px]">
+          <div className="h-[2050px] !w-full p-20 sm:h-[1800px]">
             <h2>Spendenformular</h2>
             <p>
               Ihr Browser kann keine iFrames darstellen, bitte nutzen Sie
