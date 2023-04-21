8f3336b302b62ac3da636a67427cbb6e90fff4e9
Grzegorz Leoniec <greg@app-interactive.de>
2023-04-21 15:38:01 +0200

    make reference select searchable

--- tina/components/ReferenceSelect.tsx
+++ tina/components/ReferenceSelect.tsx
@@ -1,171 +1,56 @@
-import { TinaCMS, useCMS, wrapFieldsWithMeta } from 'tinacms'
-import { Listbox, Transition } from '@headlessui/react'
-import React, { Fragment, useEffect } from 'react'
+import { useCMS, wrapFieldsWithMeta } from 'tinacms'
+import { Combobox, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useMemo, useState } from 'react'
 import {
   CheckIcon,
   ChevronUpDownIcon,
   LinkIcon,
+  ArrowRightIcon,
 } from '@heroicons/react/20/solid'
 import { cleanPath } from '../../src/lib/utils'
-
-/* @vite-ignore */
-
-const classNames = (...classes) => {
-  return classes.filter(Boolean).join(' ')
-}
-
-type Option = {
-  value: string
-  label: string
-}
-
-export interface ReferenceFieldProps {
-  label?: string
-  name: string
-  component: string
-  collections: string[]
-  options: (Option | string)[]
-}
-
-export interface ReferenceProps {
-  name: string
-  input: any
-  field: ReferenceFieldProps
-  disabled?: boolean
-  options?: (Option | string)[]
-}
-
-interface ReferenceSelectProps {
-  cms: TinaCMS
-  input: any
-  field: ReferenceFieldProps
-}
-
-interface Node {
-  id: string
-  _internalSys: {
-    title: string | null
-  }
-}
-interface OptionSet {
-  collection: string
-  edges: {
-    node: Node
-  }[]
-}
-
-interface Response {
-  collection: {
-    documents: {
-      edges: {
-        node: Node
-      }[]
-    }
-  }
-}
-
-const useGetOptionSets = (cms: TinaCMS, collections: string[]) => {
-  const [optionSets, setOptionSets] = React.useState<OptionSet[]>([])
-  const [loading, setLoading] = React.useState(true)
-
-  React.useEffect(() => {
-    const fetchOptionSets = async () => {
-      const optionSets = await Promise.all(
-        collections.map(async (collection) => {
-          try {
-            const response: Response = await cms.api.tina.request(
-              `#graphql
-            query ($collection: String!){
-              collection(collection: $collection) {
-                documents(first: -1) {
-                  edges {
-                    node {
-                      ...on Node {
-                        id,
-                      }
-                      ...on Document {
-                        _internalSys: _sys {
-                          title
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-            `,
-              { variables: { collection } }
-            )
-
-            return {
-              collection,
-              edges: response.collection.documents.edges,
-            }
-          } catch (e) {
-            return {
-              collection,
-              edges: [],
-            }
-          }
-        })
-      )
-
-      setOptionSets(optionSets)
-
-      // save optionSets to localStorage
-      localStorage.setItem('optionSets', JSON.stringify(optionSets, null, 2))
-
-      setLoading(false)
-      return
-    }
-    // set optionSets from localStorage
-    const cache = localStorage.getItem('optionSets')
-    if (!optionSets?.length && cache) {
-      setOptionSets(JSON.parse(cache))
-      setLoading(false)
-    }
-
-    if (cms && collections.length > 0) {
-      fetchOptionSets()
-    }
-  }, [cms, collections])
-
-  return { optionSets, loading }
-}
+import {
+  useFilteredOptions,
+  useReference,
+  useSelected,
+} from './hooks/ReferenceHooks'
+import clsx from 'clsx'
 
 const selectClasses =
-  'block w-full cursor-pointer appearance-none truncate rounded-md border border-gray-200 bg-white py-2 pl-3 pr-8 text-base shadow focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm'
+  'flex flex-col w-full appearance-none truncate rounded-md border border-gray-200 bg-white py-2 pl-2 pr-8 text-base shadow focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm'
+
+type ReferenceOption = {
+  id: string
+  title: string
+}
+
+function findById(
+  options: ReferenceOption[],
+  id: string
+): ReferenceOption | null {
+  return options.find((option) => option.id === id)
+}
 
 export const useReferenceSelect = wrapFieldsWithMeta(
   ({ field, input, meta }) => {
     const cms = useCMS()
-    const { optionSets, loading } = useGetOptionSets(cms, ['page'])
-    const [isMounted, setMounted] = React.useState(true)
+    const [query, setQuery] = useState<string>('')
+    const { loading, options } = useReference(cms, 'page')
+    const selected = useSelected(options, input.value)
+    const { filteredOptions, filteredStats } = useFilteredOptions(
+      options,
+      query
+    )
 
-    const [selected, setSelected] = React.useState()
     useEffect(() => {
-      if (!isMounted) return
-      if (optionSets?.length < 1) return
-
-      setSelected(input.value)
-
-      // find input.value in optionSets
-      try {
-        const title = optionSets[0].edges.find(
-          (edge) => edge?.node?.id === input?.value
-        )!.node._internalSys.title as any
-        setSelected(title as any)
-      } catch (e) {}
-
-      return () => {
-        setMounted(false)
+      if (selected) {
+        setQuery('')
       }
-    }, [input.value, loading])
+    }, [selected])
 
     if (loading === true) {
       return (
         <div
-          className={classNames(
+          className={clsx(
             selectClasses,
             'cursor-wait !bg-gray-100 text-gray-400'
           )}
@@ -176,132 +61,155 @@ export const useReferenceSelect = wrapFieldsWithMeta(
     }
 
     return (
-      <Listbox value={input.value} onChange={input.onChange}>
+      <Combobox
+        as="div"
+        value={input.value}
+        onChange={input.onChange}
+        onClick={() => {
+          setTimeout(() => {
+            setQuery('')
+          }, 250)
+        }}
+      >
         {({ open }) => (
           <>
-            {/* <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
-                            Assigned to
-                          </Listbox.Label> */}
             <div className="relative mt-2">
-              <Listbox.Button className={selectClasses}>
-                <span className="flex w-full flex-col items-start truncate leading-tight">
-                  {!selected && (
-                    <>
-                      <span className="!text-md shrink-0 truncate text-gray-300">
-                        Seite verknüpfen…
-                      </span>
-                    </>
-                  )}
-                  {selected && (
-                    <>
-                      <span className="!text-md shrink-0 truncate">
-                        {selected}
-                      </span>
-                      <span className="shrink-0 truncate !text-xs text-gray-300">
-                        {cleanPath(input.value)}
-                      </span>
-                    </>
-                  )}
-                </span>
-                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <div className={clsx(selectClasses, 'h-[53px]')}>
+                <Combobox.Input
+                  onChange={(e) => setQuery(e.target.value ?? '')}
+                  placeholder="🔍 Seite Suchen"
+                  title={
+                    selected && `${selected?.title}\n${cleanPath(input.value)}`
+                  }
+                  className="absolute inset-0 flex h-[53px] w-full rounded-md bg-transparent !pt-[15px] pb-8 pl-3 pr-10 text-start focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+                  displayValue={(id) => {
+                    setQuery('')
+                    return findById(options, id.toString())?.title
+                  }}
+                />
+                {filteredStats.isReduced && (
+                  <span className="shrink-0 select-none truncate pl-1 pt-[20px] !text-xs text-gray-300">
+                    {filteredStats.filtered > 0 && (
+                      <>
+                        {filteredStats.filtered} von {filteredStats.total}
+                      </>
+                    )}
+                    {!filteredStats.filtered && <>Keine Ergebnisse</>}
+                  </span>
+                )}
+                {!filteredStats.isReduced && selected && (
+                  <span className="shrink-0 select-none truncate pl-1 pt-[20px] !text-xs text-gray-300">
+                    <ArrowRightIcon className="mr-1 inline h-3 w-3" />
+                    {cleanPath(input.value)}
+                  </span>
+                )}
+                {!filteredStats.isReduced && !selected && (
+                  <span className="shrink-0 select-none truncate pl-1 pt-[20px] !text-xs text-gray-300">
+                    Bitte verknüpfe eine Seite
+                  </span>
+                )}
+                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
                   <ChevronUpDownIcon
                     className="h-5 w-5 text-gray-400"
                     aria-hidden="true"
                   />
-                </span>
-              </Listbox.Button>
+                </Combobox.Button>
+              </div>
 
               <Transition
                 show={open}
                 as={Fragment}
-                leave="transition ease-in duration-100"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
+                enter="transition ease-out duration-75 origin-top transform-gpu"
+                enterFrom="transform opacity-0 scale-y-[0.97]"
+                enterTo="transform opacity-100 scale-y-100"
+                leave="transition ease-in duration-75 origin-top transform-gpu"
+                leaveFrom="transform opacity-100 scale-y-100"
+                leaveTo="transform opacity-0 scale-y-[0.97]"
               >
-                <Listbox.Options className="absolute !z-[1000] mt-1 !max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                  {optionSets.length > 0 &&
-                    optionSets[0].edges.map(
-                      ({
-                        node: {
-                          id,
-                          _internalSys: { title },
-                        },
-                      }) => (
-                        <Listbox.Option
-                          key={id}
-                          className={({ active }) =>
-                            classNames(
-                              active
-                                ? 'bg-indigo-600 text-white'
-                                : 'text-gray-900',
-                              'relative cursor-default select-none py-2 pl-3 pr-14'
-                            )
-                          }
-                          value={id}
-                        >
-                          {({ selected, active }) => (
-                            <>
-                              <div className="group flex flex-col leading-tight">
-                                <span
-                                  className={classNames(
-                                    selected ? 'font-semibold' : 'font-medium',
-                                    '-mt-px truncate text-sm'
-                                  )}
-                                >
-                                  {title}
-                                </span>
-                                <span
-                                  className={classNames(
-                                    active
-                                      ? 'text-indigo-200'
-                                      : 'text-gray-300',
-                                    'truncate text-xs'
-                                  )}
-                                >
-                                  {cleanPath(id)}
-                                </span>
-                              </div>
-
-                              <a
-                                href={cleanPath(id)}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                title="Öffne in neuem Tab"
-                                className={classNames(
-                                  selected ? '' : 'hidden',
-                                  'absolute inset-y-0 right-0 flex items-center pr-4 text-white/50 hover:text-white'
+                <Combobox.Options
+                  className={clsx(
+                    'absolute !z-[1000] mt-1 !max-h-60 w-full overflow-auto rounded-md bg-white text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm',
+                    filteredOptions.length < 1 && 'hidden'
+                  )}
+                >
+                  {filteredOptions.length > 0 &&
+                    filteredOptions.map(({ id, title }) => (
+                      <Combobox.Option
+                        key={id}
+                        className={({ active }) =>
+                          clsx(
+                            active
+                              ? 'bg-indigo-600 text-white'
+                              : 'text-gray-900',
+                            'relative cursor-default select-none py-2 pl-3 pr-14'
+                          )
+                        }
+                        value={id}
+                      >
+                        {({ selected, active }) => (
+                          <>
+                            <div className="group flex flex-col leading-tight">
+                              <span
+                                className={clsx(
+                                  selected ? 'font-semibold' : 'font-medium',
+                                  '-mt-px truncate text-sm'
                                 )}
                               >
-                                <LinkIcon
+                                {title}
+                              </span>
+                              <span
+                                className={clsx(
+                                  active ? 'text-indigo-200' : 'text-gray-300',
+                                  'truncate text-xs'
+                                )}
+                              >
+                                {cleanPath(id)}
+                              </span>
+                            </div>
+
+                            <a
+                              href={cleanPath(id)}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              title="Öffne in neuem Tab"
+                              onClick={(e) => {
+                                // needed to prevent the combobox from closing
+                                e.stopPropagation()
+                              }}
+                              className={clsx(
+                                selected ? '' : 'hidden',
+                                'absolute inset-y-0 right-0 flex items-center pr-4 text-white/50 hover:text-white'
+                              )}
+                            >
+                              <LinkIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </a>
+
+                            {selected ? (
+                              <span
+                                className={clsx(
+                                  active ? '!hidden ' : 'text-indigo-600',
+                                  'absolute inset-y-0 right-0 flex items-center pr-4'
+                                )}
+                              >
+                                <CheckIcon
                                   className="h-5 w-5"
                                   aria-hidden="true"
                                 />
-                              </a>
-
-                              {selected ? (
-                                <span
-                                  className={classNames(
-                                    active ? '!hidden ' : 'text-indigo-600',
-                                    'absolute inset-y-0 right-0 flex items-center pr-4'
-                                  )}
-                                >
-                                  <CheckIcon
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                </span>
-                              ) : null}
-                            </>
-                          )}
-                        </Listbox.Option>
-                      )
-                    )}
-                </Listbox.Options>
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Combobox.Option>
+                    ))}
+                </Combobox.Options>
               </Transition>
             </div>
           </>
         )}
-      </Listbox>
+      </Combobox>
     )
   }
 )
---
+++ tina/components/hooks/ReferenceHooks.tsx
@@ -0,0 +1,198 @@
+import { SetStateAction, useEffect, useMemo, useState } from 'react'
+import { TinaCMS } from 'tinacms'
+import Fuse from 'fuse.js'
+
+const cleanPath = (path: string = '') => {
+  // replace ^content/pages/ and .mdx$
+  if (path?.trim() === '') return ''
+  try {
+    return path.replace(/^content\/pages/, '').replace(/\.mdx$/, '')
+  } catch {
+    return ''
+  }
+}
+
+interface Response {
+  collection: {
+    documents: {
+      edges: {
+        node: {
+          id: string
+          _internalSys: {
+            title: string | null
+          }
+        }
+      }[]
+    }
+  }
+}
+
+type ReferenceOption = {
+  id: string
+  title: string
+}
+
+async function fetchReferenceOptions(
+  cms: TinaCMS,
+  collection: string
+): Promise<ReferenceOption[]> {
+  const response: Response = await cms.api.tina.request(
+    `#graphql
+  query ($collection: String!){
+    collection(collection: $collection) {
+      documents(first: -1) {
+        edges {
+          node {
+            ...on Node {
+              id,
+            }
+            ...on Document {
+              _internalSys: _sys {
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  `,
+    { variables: { collection } }
+  )
+
+  return response.collection.documents.edges.map((edge) => ({
+    id: edge.node.id,
+    title: edge.node._internalSys.title,
+  }))
+}
+
+export const useReference = (cms: TinaCMS, collection: string) => {
+  const [loading, setLoading] = useState(true)
+  const [options, setOptions] = useState<ReferenceOption[]>([])
+  const { cache, setCache } = useReferenceCache(collection)
+  const isMounted = useIsMounted()
+
+  useEffect(() => {
+    if (cache.length) {
+      setOptions(cache)
+      setLoading(false)
+      return
+    }
+    fetchReferenceOptions(cms, collection).then((options) => {
+      if (!isMounted) return
+      setOptions(options)
+      setCache(options)
+      setLoading(false)
+    })
+  }, [cache, cache.length, cms, collection, isMounted, setCache])
+
+  useEffect(() => {
+    if (!cache.length) return
+    setLoading(false)
+  }, [cache, setOptions])
+
+  return {
+    loading,
+    options,
+  }
+}
+
+export const useSelected = (options: ReferenceOption[], id: string) => {
+  const [selected, setSelected] = useState<ReferenceOption | undefined>()
+
+  useEffect(() => {
+    const selected = options.find((option) => option.id === id)
+    setSelected(selected)
+  }, [options, id])
+
+  return selected
+}
+
+export const useFilteredOptions = (
+  options: ReferenceOption[],
+  search: string
+) => {
+  const [filteredOptions, setFilteredOptions] = useState<ReferenceOption[]>([])
+
+  // setup fuse search
+  const fuse = useMemo(
+    () =>
+      new Fuse(options, {
+        threshold: 0.5,
+        keys: [
+          {
+            name: 'title',
+            weight: 2,
+          },
+          {
+            name: 'id',
+            weight: 0.5,
+            getFn: (option) => cleanPath(option.id),
+          },
+        ],
+      }),
+    [options]
+  )
+
+  const [stats, setStats] = useState<{
+    total: number
+    filtered: number
+    isReduced: boolean
+  }>({
+    total: options.length,
+    filtered: filteredOptions.length,
+    isReduced: false,
+  })
+
+  useEffect(() => {
+    if (search && search.trim().length > 0) {
+      const results = fuse.search(search)
+      setFilteredOptions(results.map((result) => result.item))
+      setStats({
+        total: options.length,
+        filtered: results.length,
+        isReduced: results.length < options.length,
+      })
+    } else {
+      setFilteredOptions(options)
+      setStats({
+        total: options.length,
+        filtered: options.length,
+        isReduced: false,
+      })
+    }
+  }, [options, search, fuse])
+
+  return { filteredOptions, filteredStats: stats }
+}
+
+export const useIsMounted = () => {
+  const [mounted, setMounted] = useState(true)
+  useEffect(() => {
+    if (!mounted) return
+    return () => {
+      setMounted(false)
+    }
+  }, [mounted])
+
+  return mounted
+}
+
+export const useReferenceCache = (collection: string) => {
+  const [cache, setCache] = useState<ReferenceOption[]>([])
+
+  useEffect(() => {
+    const cached = localStorage.getItem(`reference-${collection}`)
+    if (cached) {
+      setCache(JSON.parse(cached))
+    }
+  }, [collection])
+
+  return {
+    cache,
+    setCache: (cache: ReferenceOption[]) => {
+      localStorage.setItem(`reference-${collection}`, JSON.stringify(cache))
+      setCache(cache)
+    },
+  }
+}
