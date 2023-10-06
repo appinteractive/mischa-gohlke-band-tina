c2b5d3edfa4dfcaa43702874e7fce743ce0cf1b8
Grzegorz Leoniec <greg@app-interactive.de>
2023-10-06 11:43:30 +0200

    updated social media links and sitemap

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -54,7 +54,7 @@ export function Footer({ items }: Props) {
     social: [
       {
         name: 'YouTube',
-        href: 'https://www.youtube.com/channel/UCvADF2ZMvSg5DonZ06Kf3KA',
+        href: 'https://www.youtube.com/@mischagohlkeband2171/featured',
         icon: (props) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path
@@ -65,7 +65,7 @@ export function Footer({ items }: Props) {
           </svg>
         ),
       },
-      {
+      /* {
         name: 'Instagram',
         href: 'https://www.instagram.com/grenzen_sind_relativ/',
         icon: (props) => (
@@ -77,10 +77,10 @@ export function Footer({ items }: Props) {
             />
           </svg>
         ),
-      },
+      }, */
       {
         name: 'Facebook',
-        href: 'https://www.facebook.com/grenzensindrelativ',
+        href: 'https://www.facebook.com/mischagohlkeband',
         icon: (props) => (
           <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
             <path
@@ -91,7 +91,7 @@ export function Footer({ items }: Props) {
           </svg>
         ),
       },
-      {
+      /* {
         name: 'Twitter',
         href: 'https://twitter.com/GSR_eV',
         icon: (props) => (
@@ -99,7 +99,7 @@ export function Footer({ items }: Props) {
             <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
           </svg>
         ),
-      },
+      }, */
     ],
   }
 
